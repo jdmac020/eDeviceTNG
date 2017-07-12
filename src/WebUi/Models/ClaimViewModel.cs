a03af2a400cf2da9be6e1c414bf985e73ab2b9a5
jdmac020 <jmacgregor@student.cscc.edu>
2017-07-12 18:32:33 -0400

    UI Changes
    Added table view for policyholder's claim details, trimmed table
    contents.

--- src/WebUi/Models/ClaimViewModel.cs
+++ src/WebUi/Models/ClaimViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using EDeviceClaims.Core;
 using EDeviceClaims.Domain.Models;
 using EDeviceClaims.Domain.Services;
 
@@ -13,15 +14,20 @@ namespace EDeviceClaims.WebUi.Models
         public string PolicyNumber { get; set; }
         public string DeviceName { get; set; }
         public string SerialNumber { get; set; }
-        public string Status { get; set; }
+        public string Opened { get; set; }
+        public ClaimStatus Status { get; set; }
         public List<NoteViewModel> Notes { get; set; } = new List<NoteViewModel>();
         
         public ClaimViewModel(ClaimDomainModel domainModel)
         {
             Id = domainModel.Id;
+            Opened = $"{domainModel.WhenStarted.ToShortDateString()}";
+            Status = domainModel.Status;
 
             SetPolicyProperties(domainModel.Policy);
         }
+
+        
 
         private void SetPolicyProperties(PolicyDomainModel policy)
         {
