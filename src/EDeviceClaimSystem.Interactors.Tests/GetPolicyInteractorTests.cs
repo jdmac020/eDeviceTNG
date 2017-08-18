c9458ac919039ee94d9e4a8da5fde945c0a0eae3
jdmac020 <jmacgregor@student.cscc.edu>
2017-08-18 17:24:30 -0400

    Added Additional Test Classes

---
+++ src/EDeviceClaimSystem.Interactors.Tests/GetPolicyInteractorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDeviceClaims.Interactors;
+using NUnit.Framework;
+using Shouldly;
+
+namespace EDeviceClaimSystem.Interactors.Tests
+{
+    /// <summary>
+    /// Basic unit tests using in-test object creation
+    /// </summary>
+    
+    [TestFixture]
+    public class GetPolicyInteractorTests
+    {
+        [Test]
+        public void GetPolicyById_EmptyPolicyId_ReturnsNull()
+        {
+            // Arrange the test
+            IGetPolicyInteractor interactor = new GetPolicyInteractor();
+            var emptyGuid = Guid.Empty;
+
+            // Act (run the test)
+            var result = interactor.GetById(emptyGuid);
+
+            // Assert what should happen
+            result.ShouldBe(null);
+        }
+    }
+}
